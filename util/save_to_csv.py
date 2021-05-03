ee00652f3c7d589d28bec65c389d265da69f5925
ajders <ajpofficial@gmail.com>
2021-05-03 21:51:20 +0200

    Added csv-record script

---
+++ util/save_to_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def save_result_to_csv(csv_path, data):
+    try:
+        csv_df = pd.read_csv(csv_path)
+        success = True
+    except:
+        csv_df = pd.DataFrame(data, index=[0])
+        csv_df.to_csv(csv_path, index=False)
+        success = False
+    if success:
+        next_index = len(csv_df.index)
+        new_df = pd.DataFrame(data, index=[next_index-1])
+        csv_df = csv_df.append(new_df, ignore_index = True)
+        csv_df.to_csv(csv_path, index=False)
