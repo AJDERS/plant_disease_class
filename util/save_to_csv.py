50c052bd0471e80bd5f6906d714999e0787f18ba
ajders <ajpofficial@gmail.com>
2021-05-04 13:53:32 +0200

    Added function which checks if record is in csv

--- util/save_to_csv.py
+++ util/save_to_csv.py
@@ -13,3 +13,15 @@ def save_result_to_csv(csv_path, data):
         new_df = pd.DataFrame(data, index=[next_index-1])
         csv_df = csv_df.append(new_df, ignore_index = True)
         csv_df.to_csv(csv_path, index=False)
+
+
+def record_is_in_csv(csv_path, timestamp):
+    try:
+        csv_df = pd.read_csv(csv_path)
+        success = True
+    except:
+        return False
+    if timestamp in csv_df['time-stamp'].values:
+        return True
+    else:
+        return False
