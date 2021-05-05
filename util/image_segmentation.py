76774e6f29fbccb04637a59ba772c72402250657
ajders <ajpofficial@gmail.com>
2021-05-05 15:12:23 +0200

    Change thresholding, save images along the way, minor changes

--- util/image_segmentation.py
+++ util/image_segmentation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
 from skimage.segmentation import slic
+from skimage.color import deltaE_cie76
 
 def _load_image(image_dir):
     image = Image.open(image_dir)
@@ -13,16 +14,16 @@ def _load_image(image_dir):
 
 def slic_segment(image_dir, n_segments):
     image = _load_image(image_dir)
-    segments = slic(image, n_segments)
+    segments = slic(image, n_segments, compactness=25)
     color_segments = color.label2rgb(segments, image, kind='avg')
-    save_image(color_segments)
+    save_image(color_segments, 'color_segments')
+    save_image(segments, 'segments')
     return color_segments, segments
 
 def identify_leaf_segments(color_segments, segments):
     n_segments = len(np.unique(segments))
     segment_colors = np.zeros((n_segments, 3))
-    green_array = np.zeros((n_segments, 3))
-    green_array[:] = [0,128,0]
+    green_array = np.array([0,128,0])
     found_segments = []
     for row in range(segments.shape[0]):
         for col in range(segments.shape[1]):
@@ -36,22 +37,22 @@ def identify_leaf_segments(color_segments, segments):
             if len(found_segments) == n_segments:
                 break
 
-    mse_seg_green = np.sum(((segment_colors - green_array)**2), axis=1)
+    mse_seg_green = np.apply_along_axis(lambda x: deltaE_cie76(x, green_array), -1, segment_colors) 
     # MSE of 10000 between segment color and green is experimentally set
-    leaf_seg_mask = np.ma.masked_array(mse_seg_green, mse_seg_green < 10000).mask
-    leaf_seg_index = [i for (i,s) in enumerate(leaf_seg_mask) if s]
-    return leaf_seg_index
+    leaf_seg_mask = np.ma.masked_array(mse_seg_green, mse_seg_green < 120).mask
+    leaf_seg_color = [segment_colors[i] for (i,s) in enumerate(leaf_seg_mask) if s]
+    return leaf_seg_color
 
-def minimum_bounding_box(segments, leaf_seg_index):
+def minimum_bounding_box(color_segments, leaf_seg_color):
     bounding_boxes = []
-    for leaf_index in leaf_seg_index:
-        leaf_coords = np.argwhere(segments == leaf_index)
+    for leaf_color in leaf_seg_color:
+        leaf_coords = np.argwhere(color_segments == leaf_color)
         min_x, min_y = np.min(leaf_coords[...,0]), np.min(leaf_coords[...,1])
         max_x, max_y = np.max(leaf_coords[...,0]), np.max(leaf_coords[...,1])
         bounding_boxes.append([min_x, min_y, max_x, max_y])
     return bounding_boxes
 
-def add_bounding_boxes(image_dir, bounding_boxes):
+def add_bounding_boxes(image_dir, bounding_boxes, name):
     im = Image.open(image_dir)
     fig, ax = plt.subplots()
     ax.imshow(im)
@@ -62,10 +63,10 @@ def add_bounding_boxes(image_dir, bounding_boxes):
         diff_y = max_y - min_y
         rect = patches.Rectangle((min_x, min_y), diff_x, diff_y, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(rect)
-    plt.savefig('test_2.jpeg')
+    plt.savefig(f'test_{name}.jpeg')
 
 
 
-def save_image(image_data):
+def save_image(image_data, name):
         im = Image.fromarray(image_data.astype(np.uint8))
-        im.save("test.jpeg")
+        im.save(f"{name}.jpeg")
